8a38a0f83b3e830c13988084e917c69bcc85b0eb
junia2009 <junia2009@users.noreply.github.com>
2026-04-08 01:44:07 +0900

    feat: アイコンを正面向き全身ドラゴン(翼全開×炎バックライト)に刷新

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    ============================================================ */
 'use strict';
 
-const CACHE_NAME = 'dragon-cradle-v4';
+const CACHE_NAME = 'dragon-cradle-v5';
 
 const PRECACHE_ASSETS = [
   './',
